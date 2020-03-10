7436c6d62ca6aafe336750f57e784c9f425c206d
Chad Honea <chonea@gmail.com>
2020-03-09 20:21:31 -0500

    Initial commit

---
+++ dewhurst/template.php
@@ -0,0 +1,136 @@
+<?php
+// $Id: template.php,v 1.16.2.3 2010/05/11 09:41:22 goba Exp $
+
+/**
+ * Sets the body-tag class attribute.
+ *
+ * Adds 'sidebar-left', 'sidebar-right' or 'sidebars' classes as needed.
+ */
+ 
+function phptemplate_body_attributes($is_front = false, $layout = 'none') {
+
+  if ($is_front) {
+    $body_id = $body_class = 'homepage';
+  }
+  else {
+    // Remove base path and any query string.
+    global $base_path;
+    list(,$path) = explode($base_path, $_SERVER['REQUEST_URI'], 2);
+    list($path,) = explode('?', $path, 2);
+    $path = rtrim($path, '/');
+    // Construct the id name from the path, replacing slashes with dashes.
+    $body_id = str_replace('/', '-', $path);
+    // Construct the class name from the first part of the path only.
+    list($body_class,) = explode('/', $path, 2);
+  }
+  $body_id = 'page-'. $body_id;
+  $body_class = 'section-'. $body_class;
+
+  // Use the same sidebar classes as Garland.
+  $sidebar_class = ($layout == 'both') ? 'sidebars' : "sidebar-$layout";
+
+  return " id=\"$body_id\" class=\"$body_class $sidebar_class\"";
+} 
+ 
+function phptemplate_body_class($left, $right) {
+  if ($left != '' && $right != '') {
+    $class = 'sidebars';
+  }
+  else {
+    if ($left != '') {
+      $class = 'sidebar-left';
+    }
+    if ($right != '') {
+      $class = 'sidebar-right';
+    }
+  }
+
+  if (isset($class)) {
+    print ' class="'. $class .'"';
+  }
+}
+
+/**
+ * Return a themed breadcrumb trail.
+ *
+ * @param $breadcrumb
+ *   An array containing the breadcrumb links.
+ * @return a string containing the breadcrumb output.
+ */
+function phptemplate_breadcrumb($breadcrumb) {
+  if (!empty($breadcrumb)) {
+    return '<div class="breadcrumb">'. implode(' › ', $breadcrumb) .'</div>';
+  }
+}
+
+/**
+ * Override or insert PHPTemplate variables into the templates.
+ */
+ 
+function phptemplate_preprocess_page(&$vars) {
+  if (module_exists('path')) {
+    $alias = drupal_get_path_alias(str_replace('/edit','',$_GET['q']));
+    if ($alias != $_GET['q']) {
+      $template_filename = 'page';
+      foreach (explode('/', $alias) as $path_part) {
+        $template_filename = $template_filename . '-' . $path_part;
+        $vars['template_files'][] = $template_filename;
+      }
+    }
+  }
+}
+
+/**
+ * Add a "Comments" heading above comments except on forum pages.
+ */
+function garland_preprocess_comment_wrapper(&$vars) {
+  if ($vars['content'] && $vars['node']->type != 'forum') {
+    $vars['content'] = '<h2 class="comments">'. t('Comments') .'</h2>'.  $vars['content'];
+  }
+}
+
+/**
+ * Returns the rendered local tasks. The default implementation renders
+ * them as tabs. Overridden to split the secondary tasks.
+ *
+ * @ingroup themeable
+ */
+function phptemplate_menu_local_tasks() {
+  return menu_primary_local_tasks();
+}
+
+/**
+ * Returns the themed submitted-by string for the comment.
+ */
+function phptemplate_comment_submitted($comment) {
+  return t('!datetime — !username',
+    array(
+      '!username' => theme('username', $comment),
+      '!datetime' => format_date($comment->timestamp)
+    ));
+}
+
+/**
+ * Returns the themed submitted-by string for the node.
+ */
+function phptemplate_node_submitted($node) {
+  return t('!datetime — !username',
+    array(
+      '!username' => theme('username', $node),
+      '!datetime' => format_date($node->created),
+    ));
+}
+
+/**
+ * Generates IE CSS links for LTR and RTL languages.
+ */
+function phptemplate_get_ie_styles() {
+  global $language;
+
+  $iecss = '<link type="text/css" rel="stylesheet" media="all" href="'. base_path() . path_to_theme() .'/fix-ie.css" />';
+  if ($language->direction == LANGUAGE_RTL) {
+    $iecss .= '<style type="text/css" media="all">@import "'. base_path() . path_to_theme() .'/fix-ie-rtl.css";</style>';
+  }
+
+  return $iecss;
+}
